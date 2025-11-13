2c629d8e568b45c4d85c429241d5a5c8816affe5
John Christopher Olalia <152455566+zxcxtopher15@users.noreply.github.com>
2025-11-13 21:11:26 +0800

    Update blotteradmin.php

--- blotteradmin.php
+++ blotteradmin.php
@@ -152,7 +152,7 @@ function sidepanel($google_picture, $google_name) {
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
                 <span class="sidebar-text ml-3">Dashboard</span>
             </a>
-            <a href="blotter.php" class="nav-link flex items-center px-4 py-3 rounded-lg text-left font-medium ' . ($currentPage === "blotteradmin.php" ? $activeClasses : $inactiveClasses) . '" ' . $blotterClick . '>
+            <a href="blotteradmin.php" class="nav-link flex items-center px-4 py-3 rounded-lg text-left font-medium ' . ($currentPage === "blotteradmin.php" ? $activeClasses : $inactiveClasses) . '" ' . $blotterClick . '>
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
                 <span class="sidebar-text ml-3">Blotter</span>
             </a>
