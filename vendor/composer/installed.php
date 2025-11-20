4f34fdc18435e1de898230816788c7d49bb80023
John Christopher Olalia <152455566+zxcxtopher15@users.noreply.github.com>
2025-11-20 22:16:29 +0800

    a

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'soniabisquera/barangay-blotter-form',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '17f322ae927c211fbfde9735980aa9458a947e80',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v7.0.0',
+            'version' => '7.0.0.0',
+            'reference' => 'c7111310c6116ba508a6a170a89eaaed2129bd42',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'soniabisquera/barangay-blotter-form' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '17f322ae927c211fbfde9735980aa9458a947e80',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
